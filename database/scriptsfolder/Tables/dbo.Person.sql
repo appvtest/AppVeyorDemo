f7ad7e565f1d6ecf9e9f26ae8335b609bdbf8340
jtri <jtriphahn@repayonline.com>
2016-06-08 15:17:39 -0700

    Created initial database

---
+++ database/scriptsfolder/Tables/dbo.Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Person]
+(
+[Name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Age] [smallint] NULL,
+[Weight] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Height] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
